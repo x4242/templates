8326ecb5620e99c40fa63c4f49e0a34a85a7972e
0x4242 <0x4242@protonmail.com>
2020-02-23 17:15:28 +0100

    templates for sh scripts and Dockerfile(s)

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# ----------------------------------------
+# Description
+# ----------------------------------------
+# tbd
+#
+# ----------------------------------------
+# Social
+# ----------------------------------------
+# Web: http://0x4242.net
+# Twitter: @0x4242 <https://twitter.com/0x4242>
+# GitHub: x4242 <https://github.com/x4242>
+#
+# ----------------------------------------
+# Change History
+# ----------------------------------------
+# lastmod: 2020-02-21T19:59:59+01:00
+# changelog:
+#   - 2020-02-21: created
+
+ARG base_image_version:latest
+FROM alpine:$base_image_version
+
+LABEL maintainer="0x4242"
---
+++ template.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#
+# ----------------------------------------
+# Description
+# ----------------------------------------
+# Template for shell scripts.
+#
+# ----------------------------------------
+# License
+# ----------------------------------------
+# Copyright (C) 2020 0x4242
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful,but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ----------------------------------------
+# Social
+# ----------------------------------------
+# Web: http://0x4242.net
+# Twitter: @0x4242 <https://twitter.com/0x4242>
+# GitHub: x4242 <https://github.com/x4242>
+#
+# ----------------------------------------
+# Change History
+# ----------------------------------------
+# lastmod: 2020-01-27T00:01:35+01:00
+# changelog:
+#   - 2020-02-21: GPLv3
+#   - 2020-01-26: created
+
+##########################################
+# Function of Section Description Title
+# ----------------------------------------
+# tbd
+##########################################
