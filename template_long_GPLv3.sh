da2b4453141667e24f6209b43b61174231203a20
0x4242 <0x4242@protonmail.com>
2020-05-31 11:38:41 +0200

    updates

---
+++ template_long_GPLv3.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+#
+# ------------------------------------------------------------------------------
+# Description
+# ------------------------------------------------------------------------------
+# Template for shell scripts.
+#
+# ------------------------------------------------------------------------------
+# License
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020 0x4242
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful,but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ------------------------------------------------------------------------------
+# Social
+# ------------------------------------------------------------------------------
+#              __           __ __       ___    __ __       ___
+#            /'__`\        /\ \\ \    /'___`\ /\ \\ \    /'___`\
+#           /\ \/\ \  __  _\ \ \\ \  /\_\ /\ \\ \ \\ \  /\_\ /\ \
+#           \ \ \ \ \/\ \/'\\ \ \\ \_\/_/// /__\ \ \\ \_\/_/// /__
+#            \ \ \_\ \/>  </ \ \__ ,__\ // /_\ \\ \__ ,__\ // /_\ \
+#             \ \____//\_/\_\ \/_/\_\_//\______/ \/_/\_\_//\______/
+#              \/___/ \//\/_/    \/_/  \/_____/     \/_/  \/_____/
+#
+#     Web: http://0x4242.net | Twitter: @0x4242 <https://twitter.com/0x4242>
+#                 GitHub: x4242 <https://github.com/x4242>
+#
+# ------------------------------------------------------------------------------
+# Change History
+# ------------------------------------------------------------------------------
+# lastmod: 2020-03-26T17:09:19+01:00
+# changelog:
+#   - 2020-03-26: new header format + social section
+#   - 2020-02-21: GPLv3
+#   - 2020-01-26: created
+
+# ------------------------------------------------------------------------------
+# Function of Section Description Title
+# ------------------------------------------------------------------------------
+# tbd
+# ------------------------------------------------------------------------------
